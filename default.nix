cbb311a0a26861cc7371761c8a8967fb656960d5
jiriks74 <jiri@stefka.eu>
2025-01-19 07:45:53 +0100

    fix(nix): Remove unnecessary package group, use python 3.11

--- default.nix
+++ default.nix
@@ -5,12 +5,13 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
 in pkgs.mkShell {
   packages = [
-    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+    (pkgs.python311.withPackages (python-pkgs: with python-pkgs; [
       # select Python packages here
-      python-pkgs.python-lsp-server
-      python-pkgs.python-lsp-ruff
-      python-pkgs.pylsp-mypy
-      python-pkgs.pylsp-rope
+      python-lsp-server
+      python-lsp-ruff
+      pylsp-mypy
+      pylsp-rope
+      requests
     ]))
   ];
 }
